ef201b3113908e3c79de3c41e3626bea6aa30032
Kavin-crew <abreganakavin@gmail.com>
2025-04-23 22:56:46 +0800

    init files

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "830251";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1077440";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1057281";
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "61276";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
